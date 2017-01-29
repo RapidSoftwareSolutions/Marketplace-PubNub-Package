3e868fd15f57daaf571524761878928665635090
Dima Shirokov <dima.shirokoff@rapidapi.com>
2017-01-29 12:30:54 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ global.PACKAGE_NAME = "PubNub";
 
 global.RapidError = function(code, fields) {
     let messages = {
+        'M_KEYS':                 'Mismatching subscription keys.',
         'REQUIRED_FIELDS':        'Please, check and fill in required fields.',
         'JSON_VALIDATION':        'Syntax error. Incorrect input JSON. Please, check fields with JSON input.',
         'INTERNAL_PACKAGE_ERROR': 'Something went wrong inside the package.'
