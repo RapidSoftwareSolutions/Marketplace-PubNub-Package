761bad40a9a8a16d77c36172bd8aae3f5d85ecf3
Dima Shirokov <dima.shirokoff@rapidapi.com>
2017-01-30 11:23:32 +0200

    fixes

--- api/presence.js
+++ api/presence.js
@@ -1,15 +1,16 @@
-const Q = require('Q');
+const Q = require('q');
 
 module.exports = (req, res) => {
     const deferred = Q.defer();
     const { body, params } = req.body.args;
 
-    if (params.sub_key !== body.sub_key) throw new RapidError('M_KEYS');
+    if ( (!params.sub_key || !body.sub_key) || (params.sub_key !== body.sub_key) ) 
+    	throw new RapidError('M_KEYS');
 
-    const resp = JSON.stringify({
-        http_resp: "",
+    const resp = {
+        http_resp:  '',
         client_msg: body
-    });
+    };
 
     deferred.resolve(resp);
     return deferred.promise;
